edb55b3052f8ca5f8d559af4677f09687ebefd1a
Matt <155153216+Mattrick08@users.noreply.github.com>
2026-06-26 00:56:18 +0400

    Create engine_original.py

---
+++ engine_original.py
@@ -0,0 +1,149 @@
+import chess
+
+# Piece square tables - bonus points for good positions
+PAWN_TABLE = [
+     0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+     5,  5, 10, 25, 25, 10,  5,  5,
+     0,  0,  0, 20, 20,  0,  0,  0,
+     5, -5,-10,  0,  0,-10, -5,  5,
+     5, 10, 10,-20,-20, 10, 10,  5,
+     0,  0,  0,  0,  0,  0,  0,  0
+]
+
+KNIGHT_TABLE = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50
+]
+
+BISHOP_TABLE = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20
+]
+
+ROOK_TABLE = [
+     0,  0,  0,  0,  0,  0,  0,  0,
+     5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+     0,  0,  0,  5,  5,  0,  0,  0
+]
+
+QUEEN_TABLE = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+      0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+]
+
+KING_TABLE = [
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+     20, 20,  0,  0,  0,  0, 20, 20,
+     20, 30, 10,  0,  0, 10, 30, 20
+]
+
+PIECE_TABLES = {
+    chess.PAWN: PAWN_TABLE,
+    chess.KNIGHT: KNIGHT_TABLE,
+    chess.BISHOP: BISHOP_TABLE,
+    chess.ROOK: ROOK_TABLE,
+    chess.QUEEN: QUEEN_TABLE,
+    chess.KING: KING_TABLE
+}
+
+PIECE_VALUES = {
+    chess.PAWN: 100,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.ROOK: 500,
+    chess.QUEEN: 900,
+    chess.KING: 0
+}
+
+def get_position_bonus(piece_type, square, color):
+    table = PIECE_TABLES[piece_type]
+    if color == chess.WHITE:
+        index = (7 - chess.square_rank(square)) * 8 + chess.square_file(square)
+    else:
+        index = chess.square_rank(square) * 8 + chess.square_file(square)
+    return table[index]
+
+def evaluate(board):
+    if board.is_checkmate():
+        if board.turn == chess.WHITE:
+            return -9999
+        else:
+            return 9999
+
+    if board.is_stalemate() or board.is_insufficient_material():
+        return 0
+
+    score = 0
+
+    for piece_type in PIECE_VALUES:
+        for square in board.pieces(piece_type, chess.WHITE):
+            score += PIECE_VALUES[piece_type]
+            score += get_position_bonus(piece_type, square, chess.WHITE)
+        for square in board.pieces(piece_type, chess.BLACK):
+            score -= PIECE_VALUES[piece_type]
+            score -= get_position_bonus(piece_type, square, chess.BLACK)
+
+    return score
+
+def search(board, depth, alpha=-99999, beta=99999):
+    if depth == 0 or board.is_game_over():
+        return evaluate(board), None
+
+    best_move = None
+
+    if board.turn == chess.WHITE:
+        best_score = -99999
+        for move in board.legal_moves:
+            board.push(move)
+            score, _ = search(board, depth - 1, alpha, beta)
+            board.pop()
+            if score > best_score:
+                best_score = score
+                best_move = move
+            alpha = max(alpha, best_score)
+            if beta <= alpha:
+                break
+    else:
+        best_score = 99999
+        for move in board.legal_moves:
+            board.push(move)
+            score, _ = search(board, depth - 1, alpha, beta)
+            board.pop()
+            if score < best_score:
+                best_score = score
+                best_move = move
+            beta = min(beta, best_score)
+            if beta <= alpha:
+                break
+
+    return best_score, best_move
